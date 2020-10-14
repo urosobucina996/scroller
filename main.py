60192bf15c675a8b559cc2ffa0efa4e1a3730114
Uros Obucina <obucina.uros.1@gmail.com>
2020-10-14 13:20:45 +0200

    Keyboard interrupt

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import webbrowser 
+import pyautogui
 
-url = 'http://www.google.com'
+url = 'http://www.9gag.com'
 
 # new=1 opens in new browser, open otvara url
-webbrowser.open(url, new=1)
+# webbrowser.register('mozilla',webbrowser.get('mozilla'))
+width, height= pyautogui.size()
+try:
+    pyautogui.moveTo(width/2,height/2)
+    webbrowser.open(url,new=1)
+except KeyboardInterrupt:
+    print("Your script executed!")
