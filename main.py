eaa5de5ce66c6af3ca0e7df74fc58101417f0941
Serverside <obucina.uros.1@gmail.com>
2020-12-07 10:47:37 +0100

    first try of scrolling

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-import webbrowser 
-import pyautogui
+# import webbrowser 
+# import pyautogui
 
-url = 'http://www.9gag.com'
+# url = 'http://www.9gag.com'
 
-# new=1 opens in new browser, open otvara url
-# webbrowser.register('mozilla',webbrowser.get('mozilla'))
-width, height= pyautogui.size()
-try:
-    pyautogui.moveTo(width/2,height/2)
-    webbrowser.open(url,new=1)
-except KeyboardInterrupt:
-    print("Your script executed!")
+# # new=1 opens in new browser, open otvara url
+# # webbrowser.register('mozilla',webbrowser.get('mozilla'))
+# width, height= pyautogui.size()
+# try:
+#     pyautogui.moveTo(width/2,height/2)
+#     webbrowser.open(url,new=1)
+# except KeyboardInterrupt:
+#     print("Your script executed!")
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+def main():
+    
+    browser=webdriver.Chrome(ChromeDriverManager().install())
+    browser.get("http://www.9gag.com")
+    for i in range(1,10):
+        browser.execute_script("window.scrollTo(0,"+str(i*300)+")")
+        time.sleep(3)
+
+    browser.close()
+
+
+# browser=webdriver.Chrome(ChromeDriverManager().install())
+# browser.get("http://www.9gag.com")
+# browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+# time.sleep(3)
+#browser.close()
+
+if __name__ == '__main__':
+    main()
