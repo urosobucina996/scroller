099d18516cab1a99d595507603d6ba69ec8a7f1b
Serverside <obucina.uros.1@gmail.com>
2020-12-07 10:48:40 +0100

    scroller clean code

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-# import webbrowser 
-# import pyautogui
 
-# url = 'http://www.9gag.com'
-
-# # new=1 opens in new browser, open otvara url
-# # webbrowser.register('mozilla',webbrowser.get('mozilla'))
-# width, height= pyautogui.size()
-# try:
-#     pyautogui.moveTo(width/2,height/2)
-#     webbrowser.open(url,new=1)
-# except KeyboardInterrupt:
-#     print("Your script executed!")
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import time
@@ -24,13 +12,6 @@ def main():
         time.sleep(3)
 
     browser.close()
-
-
-# browser=webdriver.Chrome(ChromeDriverManager().install())
-# browser.get("http://www.9gag.com")
-# browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-# time.sleep(3)
-#browser.close()
 
 if __name__ == '__main__':
     main()
