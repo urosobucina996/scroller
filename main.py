e16545c081a0136fe9a0121c350020bd80f22ced
Serverside <obucina.uros.1@gmail.com>
2020-12-07 11:02:04 +0100

    Different type of scrolling

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 def main():
-    
+
     browser=webdriver.Chrome(ChromeDriverManager().install())
     browser.get("http://www.9gag.com")
     for i in range(1,10):
         browser.execute_script("window.scrollTo(0,"+str(i*300)+")")
+        #browser.execute_script("window.scrollBy(0,"+str(i*300)+")")
         time.sleep(3)
 
     browser.close()
