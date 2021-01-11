45019c9be3aeff1c16fc28464639af1fc9b5ab41
urosobucina996 <obucina.uros.1@gmail.com>
2021-01-11 11:29:49 +0100

    Smooth transition throught page

--- main.py
+++ main.py
@@ -2,14 +2,17 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+import json
+import os
 
 def main():
 
-    browser=webdriver.Chrome(ChromeDriverManager().install())
+    browser = webdriver.Chrome(ChromeDriverManager().install())
     browser.get("http://www.9gag.com")
+    scroll = { "top" : 0, "behavior" : 'smooth'}
     for i in range(1,10):
-        browser.execute_script("window.scrollTo(0,"+str(i*300)+")")
-        #browser.execute_script("window.scrollBy(0,"+str(i*300)+")")
+        scroll["top"] = i*350
+        browser.execute_script("window.scrollTo("+json.dumps(scroll)+")")
         time.sleep(3)
 
     browser.close()
